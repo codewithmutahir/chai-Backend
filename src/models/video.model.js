a7727fa95cd42f5767a50507aa8c535ebd7d216a
codewithmutahir <mutharsoomro13@gmail.com>
2024-06-03 10:01:20 +0500

    added user & video model

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const VideoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //Cloudinary Url
+      required: true,
+    },
+    thumbnail: {
+      type: String, //Cloudinary Url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, //Cloudinary Url
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+
+  },
+  { timeseries: true },
+);
+
+VideoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", VideoSchema);
